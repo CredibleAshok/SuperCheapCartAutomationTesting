1341c2bd1bfba9bcced163790a1d645b5463a3da
Ashok Tewatia <ashok.crediblesoftware@gmail.com>
2018-03-09 16:16:32 +0800

    Add files via upload

---
+++ SuperCheapCartAutomationTesting/SuperCheapCartAutomationTesting/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SuperCheapCartAutomationTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyFirstTest();
+            Console.WriteLine("Congrats");
+        }
+
+        //todo: remove this static keyword and create a proper architecture for test cases.
+        public static void MyFirstTest()
+        {
+            var driver = new ChromeDriver(@"C:\Dev\Projects\SuperCheapCartAutomationTesting\SuperCheapCartAutomationTesting\dependencies");
+            driver.Navigate().GoToUrl("https://www.google.com.au/");
+            var v = driver.FindElement(By.Name("q"));
+            v.SendKeys("tsa");
+            v.Submit();
+            Assert.assertEquals(null, driver.FindElement(By.LinkText("TSA - YouTube")));
+            
+        }
+    }
+}
